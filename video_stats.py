5618d5f01d4518f04cf27220342abcde4d03f087
Sameer Gaddam <gsameer2774@gmail.com>
2026-02-15 11:35:49 +0530

    Worked on getting the video_details from extract_video_data function

--- video_stats.py
+++ video_stats.py
@@ -71,8 +71,49 @@ def get_video_ids(playlist_id,limit = 500):
     except Exception as e:
         print(f"An error occurred: {e}")
         return video_ids
+    
+
+
+def extract_video_data(video_ids):
+    video_data = []
+
+    def batch_list(video_id_lst,batch_size):
+        for video_id in range(0,len(video_id_lst),batch_size):
+            yield video_id_lst[video_id:video_id+batch_size]
+
+    try:
+        for batch in batch_list(video_ids,maxResults):
+            video_ids_str = ','.join(batch)
+
+            url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}'
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('items',[]):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+            
+                video_item = {
+                    "video_id": video_id,
+                    "title": snippet['title'],
+                    "publishedAt": snippet['publishedAt'],
+                    "duration": contentDetails['duration'],
+                    "viewCount": statistics.get('viewCount', 0),
+                    "likeCount": statistics.get('likeCount',0),
+                    "commentCount": statistics.get('commentCount',0)    
+                }
+                video_data.append(video_item)
+        return video_data
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return video_data
 
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
-    print(f"playlist_id is:{playlist_id}")
-    print(f"Video ids are: {get_video_ids(playlist_id)}")
+    video_ids = get_video_ids(playlist_id)
+    video_data = extract_video_data(video_ids)
+    print(video_data)
